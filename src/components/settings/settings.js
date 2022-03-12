49d13a0db4b0832d914d3ebfc5f808b36074745f
Perla-111 <kalyankumar111.perla@gmail.com>
2022-03-12 22:02:22 +0530

    post finishing settings page

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,31 +1,87 @@
 import './settings.css';
 
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import DataUsageIcon from '@material-ui/icons/DataUsage';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import CropFreeIcon from '@material-ui/icons/CropFree';
+import {IconButton} from '@material-ui/core';
+
 import {withRouter} from 'react-router-dom';
 
 const Settings = (props) =>{
     return(
         <div className='settingsPage'>
             <div className='settingsHeader'>
-                <label className='backIcon' onClick={()=>{props.history.push('/home')}}>back icon</label>
-                <label>Settings</label>
+                <label className='backIcon' onClick={()=>{props.history.push('/home')}}>
+                    <IconButton>
+                    <ArrowBackIcon style={{color:'whitesmoke',fontSize:'30px'}}></ArrowBackIcon>
+                    </IconButton></label>
+                <label style={{color:'white',fontWeight:'600',fontSize:'22px'}}>Settings</label>
             </div>
             <div className='settingsProfile'>
-                <label>Profile Name </label>
-                <label>icon here</label>
+                <label className='settingsStatus'>
+                    <label><AccountCircleIcon style={{fontSize:'80px',color:'lightgrey'}} /></label>
+                <label className='settingsStatusName'>
+                    <label style={{fontSize:'20px',fontWeight:'bold'}}>Name here</label>
+                <label>Hey there! I am using WhatsApp</label>
+                </label>
+                </label>
+
+                <label><CropFreeIcon stle={{color:'#128C7E'}} /></label>
             </div>
+            <hr/>
             <div className='settingsBody'>
-                <div className="bodycontent">
-                    <label>content 1</label>
-                    <label>content 2</label>
-                </div>
-                <div className="bodycontent">
-                    <label>content 1</label>
-                    <label>content 2</label>
-                </div>
+            <label className='body'>
+                    <label style={{paddingLeft:'20px'}}><VpnKeyIcon style={{fontSize:'30px',color:'grey'}} /></label>
+                <label className='bodyName'>
+                    <label style={{fontSize:'20px',fontWeight:'bold'}}>Name here</label>
+                <label>Hey there! I am using WhatsApp</label>
+                </label>
+                </label>
+                <label className='body'>
+                    <label style={{paddingLeft:'20px'}}><ChatIcon style={{fontSize:'30px',color:'grey'}} /></label>
+                <label className='bodyName'>
+                    <label style={{fontSize:'20px',fontWeight:'bold'}}>Name here</label>
+                <label>Hey there! I am using WhatsApp</label>
+                </label>
+                </label>
+                <label className='body'>
+                    <label style={{paddingLeft:'20px'}}><NotificationsIcon style={{fontSize:'30px',color:'grey'}} /></label>
+                <label className='bodyName'>
+                    <label style={{fontSize:'20px',fontWeight:'bold'}}>Name here</label>
+                <label>Hey there! I am using WhatsApp</label>
+                </label>
+                </label>
+                <label className='body'>
+                    <label style={{paddingLeft:'20px'}}><DataUsageIcon style={{fontSize:'30px',color:'grey'}} /></label>
+                <label className='bodyName'>
+                    <label style={{fontSize:'20px',fontWeight:'bold'}}>Name here</label>
+                <label>Hey there! I am using WhatsApp</label>
+                </label>
+                </label>
+                <label className='body'>
+                    <label style={{paddingLeft:'20px'}}><HelpOutlineIcon style={{fontSize:'30px',color:'grey'}} /></label>
+                <label className='bodyName'>
+                    <label style={{fontSize:'20px',fontWeight:'bold'}}>Name here</label>
+                <label>Hey there! I am using WhatsApp</label>
+                </label>
+                </label>
+                <label className='body'>
+                    <label style={{paddingLeft:'20px'}}><SupervisorAccountIcon style={{fontSize:'30px',color:'grey'}} /></label>
+                <label className='bodyName'>
+                    <label style={{fontSize:'20px',fontWeight:'bold'}}>Name here</label>
+                </label>
+                </label>
             </div>
             <div className='settingsFooter'>
-                <label className='footerheader'>from</label><br/>
-                <label className='footerBrand'>icon Meta</label>
+                <label className='footerheader' >from</label><br/>
+                <label className='footerBrand'><AllInclusiveIcon /> Meta</label>
             </div>
         </div>
     );
